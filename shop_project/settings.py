5063ba86f3818f40b081d73d35c39e5d1c918e93
shagormukthakim <130634052+shagormukthakim@users.noreply.github.com>
2024-12-01 00:45:57 +0600

    Add .gitignore to ignore unnecessary files

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
